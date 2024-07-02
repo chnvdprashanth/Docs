8c17949934c041588c9ba6248f35f224418753a8
nvd70 <chnvdprashanth@gmail.com>
2024-07-02 19:08:16 +0530

    Added Delete, Edit functionality to cards ( Note), File Size, Back option in Edit Page. Used React Icons, uuid package.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,34 +4,40 @@ import { LuDownload } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+import { getDataFromLocalStorage } from "../utils/data";
 
-const Card = ({ reference, note, index }) => {
-  const [downloadState, setdownloadState] = useState(true);
-  const [downloadTagState, setdownloadTagState] = useState(false);
+const Card = ({ reference, note, id, setNotes }) => {
+  const [isDownloadActive, setIsDownloadActive] = useState(true);
+  const [isDownloadTagVisible, setIsDownloadTagVisible] = useState(false);
   const byteSize = (str) => new Blob([str]).size;
   const handleDownload = () => {
-    if (!downloadState) {
-      setdownloadState(true);
-      setdownloadTagState(false);
+    if (!isDownloadActive) {
+      setIsDownloadActive(true);
+      setIsDownloadTagVisible(false);
     } else {
-      setdownloadState(false);
-      setdownloadTagState(true);
+      setIsDownloadActive(false);
+      setIsDownloadTagVisible(true);
     }
   };
 
   const handleDownloadFile = () => {
-    const note_blob = new Blob([note.desc],{ type : 'text/plain' })
-    console.log(note_blob)
-    const link = document.createElement('a')
+    const note_blob = new Blob([note.desc], { type: "text/plain" });
+    const link = document.createElement("a");
 
-    link.href = URL.createObjectURL(note_blob)
-    console.log()
-    link.download = `${note.title}.txt`
+    link.href = URL.createObjectURL(note_blob);
+    link.download = `${note.title}.txt`;
 
-    link.click()
+    link.click();
 
-    URL.revokeObjectURL(link.href)
-  }
+    URL.revokeObjectURL(link.href);
+  };
+
+  const handleDeleteNote = (e) => {
+    localStorage.removeItem(document.getElementById(id).id);
+    setNotes(getDataFromLocalStorage());
+  };
 
   return (
     <motion.div
@@ -41,18 +47,33 @@ const Card = ({ reference, note, index }) => {
       dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
       className="relative w-52 h-64 rounded-[40px] bg-zinc-900/90 text-white p-5 overflow-hidden font-semibold"
     >
-      <Link to={`/edit/${index}`}>
+      <div className="flex justify-between items-center">
         <FaFileAlt />
-      </Link>
-      <div className="w-full h-24 mt-2 text-ellipsis overflow-hidden ...">
-        <p className="text-sm leading-snug">{`${note.desc}`}</p>
+        <div className="flex items-center gap-3 text-[20px]">
+          <MdDeleteOutline
+            className="text-[21px]"
+            onClick={handleDeleteNote}
+            id={id}
+          />
+          <Link to={`/edit/${id}`}>
+            <FaRegEdit />
+          </Link>
+        </div>
+      </div>
+      <div className="w-full h-[120px] mt-2  overflow-hidden ...">
+        <h3 className="underline mb-2 capitalize">{`${note.title}`}</h3>
+        <p className="text-sm leading-snug overflow-hidden text-ellipsis display-[-webkit-box] -webkit-box-orient[vertical] -webkit-line-clamp-2">{`${note.desc}`}</p>
       </div>
       <div className="footer absolute w-full bottom-0 left-0">
         <div className="flex justify-between items-center py-4 px-5">
           <h5>{`
-                ${ (byteSize(note.desc) / (1024 * 1024)).toFixed(3) } mb
+                ${
+                  byteSize(note.desc) / (1024 * 1024) < 1
+                    ? `${(byteSize(note.desc) / 1024).toFixed(3)} KB`
+                    : `${(byteSize(note.desc) / (1024 * 1024)).toFixed(3)} Mb`
+                }
             `}</h5>
-          {downloadState ? (
+          {isDownloadActive ? (
             <span onClick={handleDownload}>
               <LuDownload />
             </span>
@@ -62,9 +83,12 @@ const Card = ({ reference, note, index }) => {
             </span>
           )}
         </div>
-        {downloadTagState ? (
+        {isDownloadTagVisible ? (
           <div className="tag w-full px-5 py-3 bg-green-500 flex items-center justify-center">
-            <h3 className="text-base font-semibold cursor-default" onClick={handleDownloadFile}>
+            <h3
+              className="text-base font-semibold cursor-default"
+              onClick={handleDownloadFile}
+            >
               Download Now.
             </h3>
           </div>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,22 +1,49 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "./Card";
 import { getDataFromLocalStorage } from "../utils/data";
+import { IoMdAdd } from "react-icons/io";
+import { v4 as uuidv4 } from "uuid";
 
 const Foreground = () => {
   const ref = useRef(null);
-  const notes = getDataFromLocalStorage();
+  const [notes, setNotes] = useState([]);
+
+  const handleCreateNote = () => {
+    const newId = uuidv4();
+    const newNote = {
+      _id: `${newId}`,
+      title: "Title here",
+      desc: "Type here about notes...",
+    };
+    localStorage.setItem(newId, JSON.stringify(newNote));
+    setNotes((prevNote) => [...prevNote, newNote]);
+  };
+
+  useEffect(() => {
+    const storedData = getDataFromLocalStorage();
+    setNotes(storedData);
+  }, []);
+
   return (
-    <div
-      ref={ref}
-      className="fixed z-[3] top-0 left-0 p-3 w-full h-full flex gap-5 flex-wrap"
-    >
-        {
-          notes.map((note)=> console.log(note))
-        }
-      {notes?.map((note, index) => (
-        <Card reference={ref} note={note} index={index} key={index} />
-      ))}
-    </div>
+    <>
+      <div className="absolute z-10 right-6 top-6 p-3 rounded-md shadow-md text-2xl font-extrabold bg-teal-600 text-white">
+        <IoMdAdd onClick={handleCreateNote} />
+      </div>
+      <div
+        ref={ref}
+        className="fixed overflow-y-scroll z-[3] top-0 left-0 p-3 w-full h-full flex gap-5 flex-wrap no-scrollbar"
+      >
+        {notes?.map((note, index) => (
+          <Card
+            reference={ref}
+            note={note}
+            id={note._id}
+            key={index}
+            setNotes={setNotes}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
